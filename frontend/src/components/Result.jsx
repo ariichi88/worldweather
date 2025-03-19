0566e8494ba0c9daab4183e20432d2d8c8eaf863
ariichi88 <ariichi88@gmail.com>
2025-03-19 19:16:20 +0900

    とりあえず完成

--- frontend/src/components/Result.jsx
+++ frontend/src/components/Result.jsx
@@ -4,13 +4,17 @@ const Result = props => {
 
 	const str = JSON.stringify(props, null, 2)
 	const obj = JSON.parse(str)
+	const temp = "℃"
 
 	return (
 		<div>
 		    <div>{obj.result && obj.result.location.country}</div>
 		    <div>{obj.result && obj.result.location.name}</div>
-		    <div>{obj.result && obj.result.current.temp_c}<span>℃</span></div>
-		    <img src={obj.result && obj.result.current.condition.icon}/>
+		    <div>
+		        {obj.result && obj.result.current.temp_c}
+		        {obj.result && temp}
+		    </div>
+		    <img src={obj.result && "https:".concat(obj.result.current.condition.icon)}/>
 		    <div>{obj.result && obj.result.current.condition.text}</div>
 		</div>
 	)
