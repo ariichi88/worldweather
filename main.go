c55b5a38b0b2b97e883b3800bdaeb31c97c46e8b
ariichi88 <ariichi88@gmail.com>
2025-03-27 19:49:07 +0900

    cssの編集

--- main.go
+++ main.go
@@ -18,12 +18,11 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "Wails World Weather",
-		Width:  1024,
-		Height: 768,
+		Width:  600,
+		Height: 400,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
